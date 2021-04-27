bd9850db7a2f1ee0a711a8161ca336207b436328
Kush Patel <khpatel180@gmail.com>
2021-04-27 00:21:49 -0500

    New UI for starting activity Clearing old database code to add new code base

--- app/src/main/java/com/ualr/customcalendar/MainActivity.java
+++ app/src/main/java/com/ualr/customcalendar/MainActivity.java
@@ -17,60 +17,16 @@ import android.widget.EditText;
 public class MainActivity extends AppCompatActivity {
 
     private EditText editText;
-    private CalendarView calendarView;
     private String selectedDate;
     private SQLiteDatabase sqLiteDataBse;
     private static final String TAG = MainActivity.class.getSimpleName();
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-         editText = findViewById(R.id.editTextTextPersonName);
-         calendarView = findViewById(R.id.calendarView);
-
-        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
-            @Override
-            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
-
-                selectedDate = Integer.toString(year) + Integer.toString(month) + Integer.toString(dayOfMonth);
-                Log.d(TAG, "Here: "+selectedDate);
-                ReadDatabase(view);
-            }
-        });
-
-        try {
-
-            mySQLiteDBHandler dbHandler = new mySQLiteDBHandler(this, "CalendarDataBase", null, 1);
-            sqLiteDataBse = dbHandler.getWritableDatabase();
-            sqLiteDataBse.execSQL("CREATE TABLE EventCalendar (Date TEXT, Event TEXT)");
-        }
-        catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    public void InsertDatabase (View view) {
-        ContentValues contentValues = new ContentValues();
-        contentValues.put("Date", selectedDate);
-        contentValues.put("Event", editText.getText().toString());
-        sqLiteDataBse.insert("EventCalendar", null, contentValues);
-    }
-
-    public void ReadDatabase (View view) {
-        String query = "Select Event from EventCalendar where Date =" + selectedDate;
-        try {
-            Cursor cursor = sqLiteDataBse.rawQuery(query, null);
-            cursor.moveToFirst();
-            editText.setText(cursor.getString( 0));
-        }
-        catch (Exception e) {
-            e.printStackTrace();
-            editText.setText("");
-        }
+         editText = findViewById(R.id.editText);
 
     }
-
 }
